7bfb9548492da03333d277d7ff9111f05f53794e
yumnamanca445 <74799420+yumnamanca445@users.noreply.github.com>
2024-03-24 07:25:49 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-04f30ff6-10a4-46a1-989e-971d32cab27e/)
 
 
 echo "$curl_output" >> Update.tsx
